7dd5db23e68c0baffcafa81e1c612e0ccca1a32e
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-04 22:19:23 +0100

    create basic cli with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Assembles a powerd6 module from a directory
+    Assemble {
+        /// The path of the directory to be assembled
+        #[arg(short, long, value_name = "./")]
+        config: Option<PathBuf>,
+    },
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
