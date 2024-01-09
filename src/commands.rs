21b01bd439753ed5b11ee67452a0e2470a7df795
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-09 16:17:06 +0100

    remove assemble to allow first release

--- src/commands.rs
+++ src/commands.rs
@@ -6,18 +6,15 @@ trait Command<F: FileSystem> {
     fn execute(&self, _: &F);
 }
 
-mod assemble;
 mod initialize;
 
 #[derive(Subcommand)]
 pub(crate) enum Commands {
-    Assemble(assemble::Assemble),
     Initialize(initialize::Initialize),
 }
 
 pub(super) fn execute_command<F: FileSystem>(command: Commands, file_system: F) {
     match command {
-        Commands::Assemble(a) => a.execute(&file_system),
         Commands::Initialize(a) => a.execute(&file_system),
     }
 }
