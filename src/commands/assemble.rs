a20bfbc6056645f3b83b881b1a02868068e42e68
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-11 21:51:32 +0100

    mock code to find and load module file

--- src/commands/assemble.rs
+++ src/commands/assemble.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use clap::{arg, Args};
 use log::trace;
 
-use crate::file_system::FileSystem;
+use crate::{file_system::{FileSystem, formats::Format}, module::Module};
 
 use super::Command;
 
@@ -18,13 +18,22 @@ pub(crate) struct Assemble {
 impl<F: FileSystem> Command<F> for Assemble {
     fn execute(&self, fs: &F) {
         trace!("Executing assemble: {:#?}", &self);
-        let dir = &self.config;
-        let children = fs.get_dir_children(&dir).unwrap();
-        trace!("Processing {:?}", children);
-        trace!("Process module information");
+        let module = Module::new();
+        self.get_module_information(fs, module);
         trace!("Process author directory");
         trace!("Process content directory");
         trace!("Process schema directory");
         trace!("Finished assemble")
+    }
+}
+
+impl Assemble {
+    fn get_module_information(&self, fs: &impl FileSystem, module: Module) {
+        trace!("Process module information");
+        if let Some(module_file) = fs.has_file_named(&self.config, "module") {
+            let format = Format::from(module_file.clone());
+            trace!("Found {:?}. Using {:?} format", module_file, format);
+            module.set_module_information(format.get_data(&module_file));
+        }
     }
 }
